cc7617f913fa24c807b02b11cb4bd7f7c33300bc
simon <simon011130@gmail.com>
2024-04-11 20:55:01 +0000

    Add train scripts

--- train/eval/mmlu.sh
+++ train/eval/mmlu.sh
@@ -18,28 +18,28 @@ export IS_ALPACA_EVAL_2=False
 #     --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
 
 # MMLU 5 shot
-python3 -m eval.mmlu.run_eval \
-    --ntrain 5 \
-    --data_dir data/eval/mmlu \
-    --save_dir results/mmlu/${MODEL_NAME}-5shot \
-    --model_name_or_path $CHECKPOINT_PATH \
-    --tokenizer_name_or_path $CHECKPOINT_PATH \
-    --eval_batch_size 4 \
+# python3 -m eval.mmlu.run_eval \
+#     --ntrain 5 \
+#     --data_dir data/eval/mmlu \
+#     --save_dir results/mmlu/${MODEL_NAME}-5shot \
+#     --model_name_or_path $CHECKPOINT_PATH \
+#     --tokenizer_name_or_path $CHECKPOINT_PATH \
+#     --eval_batch_size 4 \
 
 export CUDA_VISIBLE_DEVICES=0,1
 
 # GSM8K 8 shot
-python -m eval.gsm.run_eval \
-    --data_dir data/eval/gsm/ \
-    --max_num_examples 200 \
-    --save_dir results/gsm/${MODEL_NAME}-cot-8shot \
-    --model $CHECKPOINT_PATH \
-    --tokenizer $CHECKPOINT_PATH \
-    --n_shot 8 \
-    --use_vllm
+# python3 -m eval.gsm.run_eval \
+#     --data_dir data/eval/gsm/ \
+#     --max_num_examples 1000 \
+#     --save_dir results/gsm/${MODEL_NAME}-cot-8shot \
+#     --model $CHECKPOINT_PATH \
+#     --tokenizer $CHECKPOINT_PATH \
+#     --n_shot 8 \
+#     --use_vllm
 
 # BBH
-python -m eval.bbh.run_eval \
+python3 -m eval.bbh.run_eval \
     --data_dir data/eval/bbh \
     --save_dir results/bbh/${MODEL_NAME}-cot \
     --model $CHECKPOINT_PATH \
@@ -48,27 +48,27 @@ python -m eval.bbh.run_eval \
     --use_vllm
 
 # MATH
-python3 -m eval.math.run_eval \
-    --data_dir data/eval/math \
-    --save_dir results/math/${MODEL_NAME} \
-    --model_name_or_path $CHECKPOINT_PATH \
-    --tokenizer_name_or_path $CHECKPOINT_PATH \
-    --eval_batch_size 4
+# python3 -m eval.math.run_eval \
+#     --data_dir data/eval/math \
+#     --save_dir results/math/${MODEL_NAME} \
+#     --model_name_or_path $CHECKPOINT_PATH \
+#     --tokenizer_name_or_path $CHECKPOINT_PATH \
+#     --eval_batch_size 4
     
 # CodeEval
-python3 -m eval.codex_humaneval.run_eval \
-    --data_file data/eval/codex_humaneval/HumanEval.jsonl.gz \
-    --eval_pass_at_ks 10 \
-    --unbiased_sampling_size_n 20 \
-    --temperature 0.8 \
-    --save_dir results/codex_humaneval/{$MODEL_NAME}_temp_0_8 \
-    --model $CHECKPOINT_PATH \
-    --tokenizer $CHECKPOINT_PATH \
-    --use_vllm
-
-# AlpacaEval
-python3 -m eval.alpaca_farm.run_eval \
-    --model_name_or_path $CHECKPOINT_PATH \
-    --save_dir results/alpaca_farm/${MODEL_NAME} \
-    --eval_batch_size 20 \
-    --use_vllm
+# python3 -m eval.codex_humaneval.run_eval \
+#     --data_file data/eval/codex_humaneval/HumanEval.jsonl.gz \
+#     --eval_pass_at_ks 10 \
+#     --unbiased_sampling_size_n 20 \
+#     --temperature 0.8 \
+#     --save_dir results/codex_humaneval/{$MODEL_NAME}_temp_0_8 \
+#     --model $CHECKPOINT_PATH \
+#     --tokenizer $CHECKPOINT_PATH \
+#     --use_vllm
+# 
+# # AlpacaEval
+# python3 -m eval.alpaca_farm.run_eval \
+#     --model_name_or_path $CHECKPOINT_PATH \
+#     --save_dir results/alpaca_farm/${MODEL_NAME} \
+#     --eval_batch_size 20 \
+#     --use_vllm
--- train/ft_llama_7b_new.sh
+++ train/ft_llama_7b_new.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=$1
 BASE_MODEL=/mnt/nfs/public/hf/models/meta-llama/Llama-2-7b-hf/
 OUTPUT=alpaca-llama-2-7b-hf-new
 TRAIN_TYPE=exp
-DATA_PATH=../data/alpaca-cleaned_replaced.jsonl
+DATA_PATH=data/alpaca-cleaned_replaced.jsonl
 mkdir models
 
 OUTPUT_DIR=models/${TRAIN_TYPE}-${OUTPUT}
@@ -15,7 +15,7 @@ WORLD_SIZE=2 torchrun --nproc_per_node=2 --master_port 12343  train/finetune.py 
     --data_path ${DATA_PATH} \
     --output_dir ${OUTPUT_DIR} \
     --batch_size 64 \
-    --micro_batch_size 4 \
+    --micro_batch_size 2 \
     --num_epochs 5 \
     --learning_rate 3e-4 \
     --cutoff_len 2048 \
@@ -26,6 +26,7 @@ WORLD_SIZE=2 torchrun --nproc_per_node=2 --master_port 12343  train/finetune.py 
     --train_on_inputs \
     --use_wandb True \
     --wandb_run_name ${TRAIN_TYPE}-${OUTPUT} \
-    --wandb_project self-seq
+    --wandb_project self-seq \
+    --resume_from_checkpoint models/exp-alpaca-llama-2-7b-hf-new/checkpoint-900
 
 
