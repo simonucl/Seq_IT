dfeaf4c9a8369f67bde536bc191a045d49df9593
simonucl <simon011130@gmail.com>
2024-04-14 12:58:01 +0100

    add eval rouge scripts

--- eval_seq/eval_rouge.py
+++ eval_seq/eval_rouge.py
@@ -7,44 +7,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-predict_es =[]
-#load the jsonl file
-with open('../multilingual-alpaca/commonsense_qa_pararepeatpara_llama7b-alpaca.jsonl', 'r') as file:
-    for line in file:
-        obj = json.loads(line)
-        predict_es.append(obj)
-#extract 'text' from each item of the jsonl file
-preds = []
-for i in range(len(predict_es)):
-    preds.append(predict_es[i]['output'])
+from argparse import ArgumentParser
 
-labels = []
-for i in range(len(predict_es)):
-    labels.append(predict_es[i]['input'])
-#load json file
-with open('../multilingual-alpaca/commonsense_qa_repeat_answer.json') as f:
-    lines = json.load(f)
-#extract 'output' from each item of json
+def main(args):
+    references = []
+    #load the jsonl file
+    with open(args.ref_file, 'r') as file:
+        for line in file:
+            obj = json.loads(line)
+            references.append(obj['output'])
+    #load json file
+    preds = []
+    with open(args.test_file, 'r') as file:
+        for line in file:
+            obj = json.loads(line)
+            preds.append(obj['output'])
+    #extract 'output' from each item of json
 
-#labels = []
-'''
-for i in range(len(lines)):
-    labels.append(lines[i]['input'])
-'''
-#compute the BLEU score
-smoothie = SmoothingFunction().method4
-score = 0
-for i in range(len(preds)):
-    score += sentence_bleu(labels[i], preds[i], smoothing_function=smoothie)
-print(score/len(preds))
+    #labels = []
+    '''
+    for i in range(len(lines)):
+        labels.append(lines[i]['input'])
+    '''
+    #compute the BLEU score
+    smoothie = SmoothingFunction().method4
+    score = 0
+    for i in range(len(preds)):
+        score += sentence_bleu(references[i], preds[i], smoothing_function=smoothie)
+    print(score/len(preds))
 
-from rouge import Rouge
-rouge = Rouge()
-score = 0
-for i in range(len(preds)):
-    score += rouge.get_scores(preds[i], labels[i])[0]['rouge-l']['f']
-print(score/len(preds))
-from bert_score import score
-P, R, F1 = score(preds, labels, lang="en", verbose=True)
-print(F1.mean())
+    from rouge import Rouge
+    rouge = Rouge()
+    score = 0
+    for i in range(len(preds)):
+        score += rouge.get_scores(preds[i], references[i])[0]['rouge-l']['f']
+    print(score/len(preds))
+    from bert_score import score
+    P, R, F1 = score(preds, references, lang="en", verbose=True)
+    print(F1.mean())
 #print(score/len(preds))
+    
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--test_file', type=str, default=None)
+    parser.add_argument('--ref_file', type=str, default=None)
+    args = parser.parse_args()
+    main(args)
