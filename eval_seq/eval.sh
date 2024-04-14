21a80f75e6a26992c4033fb4c81b0114e383dcf2
simonucl <simon011130@gmail.com>
2024-04-14 13:06:38 +0100

    fix bugs

--- eval_seq/eval.sh
+++ eval_seq/eval.sh
@@ -3,7 +3,7 @@ python3 eval_seq/eval_rouge.py \
     --ref_file self-seq/data/lima_500-replaced.jsonl
 
 python3 eval_seq/eval_rouge.py \
-    --test_file data/alpaca-llama-2-7b-hf-baseline/seq-it-alpaca-llama-2-7b-hf-new.jsonl \
+    --test_file data/alpaca-llama-2-7b-hf-new/seq-it-alpaca-llama-2-7b-hf-new.jsonl \
     --ref_file self-seq/data/lima_500-replaced.jsonl
 
 python3 eval_seq/eval_rouge.py \
