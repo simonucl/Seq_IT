af30e47d4c48b2b56910610e57ef5aa9e5137f8e
simonucl <simon011130@gmail.com>
2024-04-16 17:34:57 +0100

    testing

--- eval_seq/infer_eval_self_seq_vllm.sh
+++ eval_seq/infer_eval_self_seq_vllm.sh
@@ -9,7 +9,8 @@ mkdir -p data/testset
 MODEL_NAME=llama-7b
 TASK=xquad
 TRAIN_TYPE=base
-for TESTLANG in en de es ru zh
+# for TESTLANG in en de es ru zh
+for TESTLANG in en zh
 do
   TESTFILE=data/test/xquad_${TRAIN_TYPE}_${TESTLANG}.json
   python3 eval_seq/generate_batch.py \
@@ -22,16 +23,16 @@ do
     --use_vllm True
 done
 
-TRAIN_TYPE=trans
-for TESTLANG in ar vi th tr hi
-do
-  TESTFILE=data/test/xquad_${TRAIN_TYPE}_${TESTLANG}.json
-  python3 eval_seq/generate_batch.py \
-    --base_model ${MODEL_PATH} \
-    --length 512 \
-    --test_file ${TESTFILE} \
-    --batch_size 128 \
-    --save_file data/testset/${MODEL_NAME}_${TRAIN_TYPE}_${TASK}_${TESTLANG}.jsonl \
-    --load_8bit False \
-    --use_vllm True
-done
+# TRAIN_TYPE=trans
+# for TESTLANG in ar vi th tr hi
+# do
+#   TESTFILE=data/test/xquad_${TRAIN_TYPE}_${TESTLANG}.json
+#   python3 eval_seq/generate_batch.py \
+#     --base_model ${MODEL_PATH} \
+#     --length 512 \
+#     --test_file ${TESTFILE} \
+#     --batch_size 128 \
+#     --save_file data/testset/${MODEL_NAME}_${TRAIN_TYPE}_${TASK}_${TESTLANG}.jsonl \
+#     --load_8bit False \
+#     --use_vllm True
+# done
