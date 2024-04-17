a64d24745fc69fbb1133fb83235f788dbba485d4
simonucl <simon011130@gmail.com>
2024-04-18 00:10:24 +0100

    add infer seq

--- eval_seq/infer_eval_self_seq_vllm_fewshot.sh
+++ eval_seq/infer_eval_self_seq_vllm_fewshot.sh
@@ -9,25 +9,29 @@ mkdir -p data/testset
 MODEL_NAME=llama-7b
 TASK=xquad
 TRAIN_TYPE=base
-for MODEL_PATH in llama-7b
+PROMPT_TYPES=(fewshot fewshot_en fewshot_multi)
+for PROMPT_TYPE in ${PROMPT_TYPES[@]}
 do
-  MODEL_NAME=$(basename $MODEL_PATH)
-  MODEL_NAME=${MODEL_NAME}_fewshot
-
-  echo MODEL_NAME: ${MODEL_NAME}
-
-  mkdir -p eval_results/${MODEL_NAME}
-
-  for TESTLANG in en es ar el hi th de ru zh tr vi
+  for MODEL_PATH in llama-7b
   do
-    TESTFILE=data/xquad/fewshot/xquad_${TESTLANG}.jsonl
-    python3 eval_seq/generate_batch.py \
-      --base_model ${MODEL_PATH} \
-      --length 1024 \
-      --test_file ${TESTFILE} \
-      --batch_size 64 \
-      --save_file eval_results/${MODEL_NAME}/${MODEL_NAME}_${TRAIN_TYPE}_${TASK}_${TESTLANG}.jsonl \
-      --load_8bit False \
-      --use_vllm True
+    MODEL_NAME=$(basename $MODEL_PATH)
+    MODEL_NAME=${MODEL_NAME}_${PROMPT_TYPE}
+
+    echo MODEL_NAME: ${MODEL_NAME}
+
+    mkdir -p eval_results/${MODEL_NAME}
+
+    for TESTLANG in en es ar el hi th de ru zh tr vi
+    do
+      TESTFILE=data/xquad/${PROMPT_TYPE}/xquad_${TESTLANG}.jsonl
+      python3 eval_seq/generate_batch.py \
+        --base_model ${MODEL_PATH} \
+        --length 1024 \
+        --test_file ${TESTFILE} \
+        --batch_size 64 \
+        --save_file eval_results/${MODEL_NAME}/${MODEL_NAME}_${TRAIN_TYPE}_${TASK}_${TESTLANG}.jsonl \
+        --load_8bit False \
+        --use_vllm True
+    done
   done
 done
