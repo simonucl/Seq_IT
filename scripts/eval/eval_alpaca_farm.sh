01318550c325d9c496330579fea17deb20894fa2
simonucl <simon011130@gmail.com>
2024-04-26 01:32:51 +0100

    add finetune scripts

--- scripts/eval/eval_alpaca_farm.sh
+++ scripts/eval/eval_alpaca_farm.sh
@@ -1,13 +1,11 @@
-export CUDA_VISIBLE_DEVICES=0
 
-for CHECKPOINT_PATH in simonycl/self-seq-7b-1-3-new simonycl/self-seq-7b-baseline simonycl/self-seq-Llama-2-7b-hf simonycl/self-seq-alpaca-replaced-wizardlm
-do
-    MODEL_NAME=$(basename $CHECKPOINT_PATH)
-    # AlpacaEval
-    python3 -m eval.alpaca_farm.run_eval \
-        --model_name_or_path $CHECKPOINT_PATH \
-        --save_dir results/alpaca_farm/${MODEL_NAME} \
-        --eval_batch_size 20 \
-        --max_new_tokens 1024 \
-        --use_vllm
-done
+CHECKPOINT_PATH=simonycl/self-seq-Llama-2-7b-hf-new
+
+MODEL_NAME=$(basename $CHECKPOINT_PATH)
+# AlpacaEval
+python3 -m eval.alpaca_farm.run_eval \
+    --model_name_or_path $CHECKPOINT_PATH \
+    --save_dir results/alpaca_farm/${MODEL_NAME} \
+    --eval_batch_size 20 \
+    --max_new_tokens 2048 \
+    --stop_id_sequences \n\n
