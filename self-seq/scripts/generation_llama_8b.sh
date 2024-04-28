0b4722c1c791234bac26c6e3ba19a6c891abda73
simonucl <simon011130@gmail.com>
2024-04-28 14:42:21 +0100

    add genereation scripts

--- self-seq/scripts/generation_llama_8b.sh
+++ self-seq/scripts/generation_llama_8b.sh
@@ -14,6 +14,26 @@
 
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
-    --sample 20 \
+    --sample 100 \
     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B \
     --batch_size 16
+
+python3 self-seq/gpt-query.py \
+    --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
+    --sample 100 \
+    --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B \
+    --batch_size 1
+
+python3 self-seq/gpt-query.py \
+    --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
+    --sample 100 \
+    --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
+    --batch_size 1 \
+    --use_instruct
+
+python3 self-seq/gpt-query.py \
+    --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
+    --sample 100 \
+    --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B-Instruct \
+    --batch_size 16 \
+    --use_instruct
