54979808a4ad24bf38087766e09bd1a7c37ae8c9
simonucl <simon011130@gmail.com>
2024-04-29 12:23:29 +0100

    update

--- self-seq/scripts/generation_llama_70b.sh
+++ self-seq/scripts/generation_llama_70b.sh
@@ -2,12 +2,14 @@ python3 self-seq/gpt-query.py \
     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
     --sample 100 \
     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-    --batch_size 2 \
+    --batch_size 4 \
+    --load_8bit \
     --use_instruct
 
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/alpaca/alpaca-cleaned.jsonl \
     --sample 100 \
     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-    --batch_size 2 \
+    --batch_size 4 \
+    --load_8bit \
     --use_instruct
