6c39757c656dae44c412646eaafc8efe61b9ec78
simonucl <simon011130@gmail.com>
2024-04-29 12:09:52 +0100

    add scripts

--- self-seq/scripts/generation_cohere_r_plus.sh
+++ self-seq/scripts/generation_cohere_r_plus.sh
@@ -1,13 +1,15 @@
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
     --sample 200 \
-    --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B-Instruct \
-    --batch_size 16 \
+    --query /mnt/data/models/c4ai-command-r-plus \
+    --batch_size 4 \
+    --load_4bit \
     --use_instruct
 
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/alpaca/alpaca-cleaned.jsonl \
     --sample 200 \
-    --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B-Instruct \
-    --batch_size 16 \
+    --query /mnt/data/models/c4ai-command-r-plus \
+    --batch_size 4 \
+    --load_4bit \
     --use_instruct
