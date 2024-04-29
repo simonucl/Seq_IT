2caad8eb148b23b0f976c2a95bba7a2f8be5169c
simonucl <simon011130@gmail.com>
2024-04-29 12:54:43 +0100

    add generation scriptS

--- self-seq/scripts/generation_gpt-4.sh
+++ self-seq/scripts/generation_gpt-4.sh
@@ -2,9 +2,9 @@
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
     --sample 100 \
-    --query gpt-3.5-turbo 
+    --query gpt-4-turbo 
 
 python3 self-seq/gpt-query.py \
     --input_file self-seq/data/alpaca/alpaca-cleaned.jsonl \
     --sample 100 \
-    --query gpt-3.5-turbo
+    --query gpt-4-turbo
