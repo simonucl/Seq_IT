1065bf53ca47e03071cf1b8da52022a8462477fa
simonucl <simon011130@gmail.com>
2024-05-01 13:09:02 +0100

    Add scripts

--- self-seq/scripts/generation_cohere_r_plus_8bit.sh
+++ self-seq/scripts/generation_cohere_r_plus_8bit.sh
@@ -1,15 +1,16 @@
-python3 self-seq/gpt-query.py \
-    --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
-    --sample 100 \
-    --query /mnt/data/models/c4ai-command-r-plus \
-    --batch_size 2 \
-    --load_8bit \
-    --use_instruct \
-    --add_system_prompt
+INPUT_FILE=self-seq/data/alpaca/alpaca-cleaned_17267.jsonl
+
+# python3 self-seq/gpt-query.py \
+#     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
+#     --sample 100 \
+#     --query /mnt/data/models/c4ai-command-r-plus \
+#     --batch_size 2 \
+#     --load_8bit \
+#     --use_instruct \
+#     --add_system_prompt
 
 python3 self-seq/gpt-query.py \
-    --input_file self-seq/data/alpaca/alpaca-cleaned.jsonl \
-    --sample 100 \
+    --input_file $INPUT_FILE \
     --query /mnt/data/models/c4ai-command-r-plus \
     --batch_size 2 \
     --load_8bit \
--- self-seq/scripts/generation_cohere_r_plus_8bit_34493.sh
+++ self-seq/scripts/generation_cohere_r_plus_8bit_34493.sh
@@ -1,18 +1,17 @@
-INPUT_FILE=self-seq/data/alpaca/alpaca-cleaned_17267.jsonl
+INPUT_FILE=self-seq/data/alpaca/alpaca-cleaned_34493.jsonl
 
 # python3 self-seq/gpt-query.py \
 #     --input_file self-seq/data/flancot/flancot_filtered_15k.jsonl \
 #     --sample 100 \
 #     --query /mnt/data/models/c4ai-command-r-plus \
-#     --batch_size 4 \
-#     --load_4bit \
+#     --batch_size 2 \
+#     --load_8bit \
 #     --use_instruct \
 #     --add_system_prompt
-
 
 python3 self-seq/gpt-query.py \
     --input_file $INPUT_FILE \
     --query /mnt/data/models/c4ai-command-r-plus \
-    --batch_size 4 \
-    --load_4bit \
+    --batch_size 2 \
+    --load_8bit \
     --use_instruct
