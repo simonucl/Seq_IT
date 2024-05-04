ffff268ca2c896e256323912e07e75d20a05cbcb
simonucl <simon011130@gmail.com>
2024-05-04 18:09:16 +0100

    add alpaca ori

--- self-seq/scripts/generation_cohere_r_plus_split_0.sh
+++ self-seq/scripts/generation_cohere_r_plus_split_0.sh
@@ -1,4 +1,4 @@
-INPUT_FILE=self-seq/data/alpaca/alpaca-split_0.jsonl
+INPUT_FILE=self-seq/data/alpaca_original/alpaca-split_0-ori.jsonl
 
 python3 self-seq/gpt-query.py \
     --input_file $INPUT_FILE \
