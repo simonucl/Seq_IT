6ad6d78459193973fcec6984864e1417967b3c08
simon <simon011130@gmail.com>
2024-05-06 09:09:10 +0000

    push

--- self-seq/scripts/generation_cohere_r_plus_test2.sh
+++ self-seq/scripts/generation_cohere_r_plus_test2.sh
@@ -2,9 +2,8 @@ INPUT_FILE=self-seq/data/alpaca_test/alpaca_test2.jsonl
 
 python3 self-seq/gpt-query.py \
     --input_file $INPUT_FILE \
-    --query /mnt/data/models/c4ai-command-r-plus-4bit \
+    --query /mnt/data/models/c4ai-command-r-plus-GPTQ \
     --batch_size 4 \
     --use_instruct \
     --use_vllm \
-    --ignore_cache \
     --no_refinement
