4caa77a9dc6ce8d3911f87ed961480f64b32faf9
simonucl <simon011130@gmail.com>
2024-05-09 11:53:48 +0200

    add process multi

--- self-seq/data/process_multi_it.py
+++ self-seq/data/process_multi_it.py
@@ -1,34 +1,43 @@
 import json
+from argparse import ArgumentParser
+
 def process_jsonl_file(file_path):
     results = []
     with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            data = json.loads(line)
-            if 'extracted_instruction' in data:
-                final_instruction = data['extracted_instruction']
-            else:
-                final_instruction = data['instruction']
-            # final_instruction = data['final_instruction']
-            # instruction_part = final_instruction.split('<|eot_id|><|start_header_id|>user<|end_header_id|>')[-1]
-            # start_l = 3
-            # length = 59 #len('�~@~]<|eot_id|><|start_header_id|>assistant<|end_header_id|>')
-            new_data = {
-                'instruction': final_instruction,
-                'output': data['final_instruction_response'],
-                'system_prompt':data['system_prompt'],
-                'input': data['input'],
-            }
-            results.append(new_data)
+        lines = file.readlines()
+    
+    for line in lines:
+        data = json.loads(line)
+        if 'extracted_instruction' in data:
+            final_instruction = data['extracted_instruction']
+        else:
+            final_instruction = data['instruction']
+        input = data['input'].replace('Input: ', '').replace('###', '').strip("\'\"")
+        final_instruction = final_instruction.strip("\'\"“”")
+        new_data = {
+            'idx': data['idx'],
+            'instruction': final_instruction,
+            'output': data['final_instruction_response'],
+            'system_prompt':data['system_prompt'],
+            'input': data['input'],
+            'option': data['option'],
+        }
+        results.append(new_data)
     return results
 
-file_path = 'flancot/flancot_llama70b_iteration2-Meta-Llama-3-70B-Instruct-generate_instruct-refine-response-final.jsonl'
-new_data_list = process_jsonl_file(file_path)
-output_file_path = 'flancot/flancot_llama70b_iteration3.jsonl'
-idx = 0
-for item in new_data_list:
-    item['idx'] = idx
-    idx+=1
-with open(output_file_path, 'w', encoding='utf-8') as file:
-        for item in new_data_list:
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--file_path', type=str, default='flancot/flancot_llama70b_iteration2-Meta-Llama-3-70B-Instruct-generate_instruct-refine-response-final.jsonl')
+    parser.add_argument('--output_file', type=str, default='flancot/flancot_llama70b_iteration3.jsonl')
+    args = parser.parse_args()
+
+    new_data = process_jsonl_file(args.file_path, args.iteration)
+    with open(args.output_file.replace('.jsonl', '-iter.jsonl'), 'w', encoding='utf-8') as file:
+        for item in new_data:
+            json_line = json.dumps(item, ensure_ascii=False)
+            file.write(json_line + '\n')
+    with open(args.output_file, 'w', encoding='utf-8') as file:
+        for item in new_data:
+            item.pop('option')
             json_line = json.dumps(item, ensure_ascii=False)
             file.write(json_line + '\n')
