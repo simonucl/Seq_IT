d12f6b13d0f015ca77a26afc3433b47ed28f716d
simonucl <simon011130@gmail.com>
2024-05-09 10:00:35 +0200

    fix input issue

--- self-seq/data/process_multi_it.py
+++ self-seq/data/process_multi_it.py
@@ -4,15 +4,19 @@ def process_jsonl_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             data = json.loads(line)
-            final_instruction = data['final_instruction']
-            instruction_part = final_instruction.split('<|eot_id|><|start_header_id|>user<|end_header_id|>')[-1]
-            start_l = 3
-            length = 59 #len('�~@~]<|eot_id|><|start_header_id|>assistant<|end_header_id|>')
+            if 'extracted_instruction' in data:
+                final_instruction = data['extracted_instruction']
+            else:
+                final_instruction = data['instruction']
+            # final_instruction = data['final_instruction']
+            # instruction_part = final_instruction.split('<|eot_id|><|start_header_id|>user<|end_header_id|>')[-1]
+            # start_l = 3
+            # length = 59 #len('�~@~]<|eot_id|><|start_header_id|>assistant<|end_header_id|>')
             new_data = {
-                    'instruction': instruction_part[start_l:-length],
+                'instruction': final_instruction,
                 'output': data['final_instruction_response'],
                 'system_prompt':data['system_prompt'],
-                'input': '',
+                'input': data['input'],
             }
             results.append(new_data)
     return results
@@ -20,7 +24,6 @@ def process_jsonl_file(file_path):
 file_path = 'flancot/flancot_llama70b_iteration2-Meta-Llama-3-70B-Instruct-generate_instruct-refine-response-final.jsonl'
 new_data_list = process_jsonl_file(file_path)
 output_file_path = 'flancot/flancot_llama70b_iteration3.jsonl'
->>>>>>> 23f8bc6d7708febc1300dcf93ded204fa1ccf06a
 idx = 0
 for item in new_data_list:
     item['idx'] = idx
