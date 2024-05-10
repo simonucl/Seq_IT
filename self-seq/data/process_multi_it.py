903079301fb65532a5f3a7b71ed98ecc17df6966
simonucl <simon011130@gmail.com>
2024-05-10 20:12:20 +0200

    add idx

--- self-seq/data/process_multi_it.py
+++ self-seq/data/process_multi_it.py
@@ -6,7 +6,7 @@ def process_jsonl_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     
-    for line in lines:
+    for i, line in enumerate(lines):
         data = json.loads(line)
         if 'extracted_instruction' in data:
             final_instruction = data['extracted_instruction']
@@ -15,7 +15,7 @@ def process_jsonl_file(file_path):
         input = data['input'].replace('Input: ', '').replace('###', '').strip("\'\"")
         final_instruction = final_instruction.strip("\'\"“”")
         new_data = {
-            'idx': data['idx'],
+            'idx': data['idx'] if 'idx' in data else i,
             'instruction': final_instruction,
             'output': data['final_instruction_response'],
             'system_prompt':data['system_prompt'],
