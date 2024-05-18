a1da1d27b38664a226a344fe5a184f78b577cb90
simon <simon011130@gmail.com>
2024-05-18 20:14:24 +0000

    local generation

--- self-seq/scripts/generation_flancot_llama_70b.sh
+++ self-seq/scripts/generation_flancot_llama_70b.sh
@@ -3,15 +3,26 @@ INPUT_FILE=self-seq/data/flancot_extract/final_15k_data_origin.jsonl
 # python3 self-seq/extract_input.py \
 #     --input_file $INPUT_FILE \
 #     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-#     --output_file self-seq/data/flancot_extract/final_15k_data.jsonl \
+#     --output_file self-seq/data/flancot_extract_refine/final_15k_data_origin.jsonl \
 #     --batch_size 1 \
 #     --use_vllm \
 #     --use_instruct
+
+# python3 self-seq/gpt-query.py \
+#     --input_file self-seq/data/flancot_extract_refine/final_15k_data_origin-extracted-input.jsonl \
+#     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
+#     --output_file self-seq/data/flancot_extract_refine/flancot_15k_Meta-Llama-3-70B-Instruct_iter_0.jsonl \
+#     --batch_size 4 \
+#     --use_instruct \
+#     --ignore_cache \
+#     --use_vllm \
+#     --no_refinement \
+#     --direct_response 
 # 
 # python3 self-seq/gpt-query.py \
-#     --input_file self-seq/data/flancot_extract/final_15k_data-extracted-input.jsonl \
+#     --input_file self-seq/data/flancot_extract_refine/final_15k_data_origin-extracted-input.jsonl \
 #     --query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-#     --output_file self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_0.jsonl \
+#     --output_file self-seq/data/flancot_extract_refine/flancot_15k_Meta-Llama-3-70B-Instruct_iter_0.jsonl \
 #     --batch_size 4 \
 #     --use_instruct \
 #     --ignore_cache \
@@ -19,35 +30,34 @@ INPUT_FILE=self-seq/data/flancot_extract/final_15k_data_origin.jsonl
 #     --no_refinement
 
 python3 self-seq/data/process_multi_it.py \
-	--file_path self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_0-generate_instruct-refine-response-final.jsonl \
-	--output_file self-seq/data/flancot_extract/flancot_llama70b_iteration_1.jsonl
+	--file_path self-seq/data/flancot_extract_refine/flancot_15k_Meta-Llama-3-70B-Instruct_iter_0-generate_instruct-refine-response-final.jsonl \
+	--output_file self-seq/data/flancot_extract_refine/flancot_llama70b_iteration_1.jsonl
 
-python3 self-seq/gpt-query.py \
-    --input_file self-seq/data/flancot_extract/flancot_llama70b_iteration_1-iter.jsonl \
-	--query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-    --output_file self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_1.jsonl \
-    --batch_size 4 \
-    --use_instruct \
-    --ignore_cache \
-    --use_vllm \
-    --no_refinement \
-    --iteration
-
-python3 self-seq/data/process_multi_it.py \
-        --file_path self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_1-generate_instruct-refine-response-final.jsonl \
-        --output_file self-seq/data/flancot_extract/flancot_llama70b_iteration_2.jsonl
-
-python3 self-seq/gpt-query.py \
-    --input_file self-seq/data/flancot_extract/flancot_llama70b_iteration_2-iter.jsonl \
-	--query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
-    --output_file self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_2.jsonl \
-    --batch_size 4 \
-    --use_instruct \
-    --ignore_cache \
-    --use_vllm \
-    --no_refinement \
-    --iteration
+# python3 self-seq/gpt-query.py \
+#     --input_file self-seq/data/flancot_extract_refine/flancot_llama70b_iteration_1-iter.jsonl \
+# 	--query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
+#     --output_file self-seq/data/flancot_extract_refine/flancot_15k_Meta-Llama-3-70B-Instruct_iter_1.jsonl \
+#     --batch_size 4 \
+#     --use_instruct \
+#     --use_vllm \
+#     --no_refinement \
+#     --iteration
 
 python3 self-seq/data/process_multi_it.py \
-        --file_path self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_2-generate_instruct-refine-response-final.jsonl \
-        --output_file self-seq/data/flancot_extract/flancot_llama70b_iteration_3.jsonl
+        --file_path self-seq/data/flancot_extract_refine/flancot_15k_Meta-Llama-3-70B-Instruct_iter_1-generate_instruct-refine-response-final.jsonl \
+        --output_file self-seq/data/flancot_extract_refine/flancot_llama70b_iteration_2.jsonl
+# 
+# python3 self-seq/gpt-query.py \
+#     --input_file self-seq/data/flancot_extract/flancot_llama70b_iteration_2-iter.jsonl \
+# 	--query /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-70B-Instruct \
+#     --output_file self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_2.jsonl \
+#     --batch_size 4 \
+#     --use_instruct \
+#     --ignore_cache \
+#     --use_vllm \
+#     --no_refinement \
+#     --iteration
+# 
+# python3 self-seq/data/process_multi_it.py \
+#         --file_path self-seq/data/flancot_extract/flancot_15k_Meta-Llama-3-70B-Instruct_iter_2-generate_instruct-refine-response-final.jsonl \
+#         --output_file self-seq/data/flancot_extract/flancot_llama70b_iteration_3.jsonl
